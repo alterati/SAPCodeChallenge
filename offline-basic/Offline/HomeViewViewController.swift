8eeefeb557d3d5ffb21da43954205bcf00af2684
Óscar Figuerola Salas <oscar.figuerolasalas@keytree.es>
2018-02-25 16:46:40 +0100

    updated layout for "closed" status; fixed optional string sales order details header

--- offline-basic/Offline/HomeViewViewController.swift
+++ offline-basic/Offline/HomeViewViewController.swift
@@ -143,6 +143,7 @@ class HomeViewViewController: UIViewController, URLSessionTaskDelegate, UITableV
             let indexPath = sender as! IndexPath
             let order: MyPrefixSalesOrderHeader = salesOrders[indexPath.row]
             let sOviewControler = segue.destination as! SalesOrderViewController
+            sOviewControler.delegate = self
             sOviewControler.initialize(oDataModel: oDataModel!)
             sOviewControler.loadSalesOrderItems(newItem: order)
             
@@ -317,4 +318,10 @@ class HomeViewViewController: UIViewController, URLSessionTaskDelegate, UITableV
         kpiHeader = FUIKPIHeader(items: [kpiView1, kpiView2])
     }
 
+}
+
+extension HomeViewViewController: SalesOrderViewControllerDelegate {
+    func didUpdateSalesOrder(_ salesOrder: MyPrefixSalesOrderHeader) {
+        self.loadData()
+    }
 }
